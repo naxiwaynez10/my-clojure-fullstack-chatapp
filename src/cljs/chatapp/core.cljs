ef5b734f2ff15d432837831d2f5f8565a6411360
naxiwaynez10 <ibrahimnasiru17@gmail.com>
2022-06-06 16:53:09 +0100

    Just a little fix

--- src/cljs/chatapp/core.cljs
+++ src/cljs/chatapp/core.cljs
@@ -1,21 +1,18 @@
 (ns chatapp.core
-  (:require [reagent.core :as r]
-            [reagent.dom :as rdom]
-            [ajax.core :refer [GET, POST]]
-            [clojure.core.async :as async :refer [timeout go]]))
+  (:require [chatapp.components.top-menu :as top-menu]
+  [chatapp.components.left-side-menu :as left-menu]
+            [clojure.core.async :as async :refer [timeout]]
+            [reagent.dom :as rdom]))
 
-(defn loader []
-  
-   (let [resp (GET "/" {:handler (fn [response] response)})]
-     (.log js/console resp))
-     [:div.bouncing-loader [:div ][:div ][:div ]])
-
+(defn baseview []
+  [:<>
+   [left-menu/main]
+   [top-menu/main]])
 
 (defn ^:dev/after-load mount-components []
-  (rdom/render [loader] (.getElementById js/document "app"))
+  (rdom/render [baseview] (.getElementById js/document "app"))
   )
 
 (defn init! []
-  
-  (timeout 20000)
+  ;; (timeout 20000)
   (mount-components))
